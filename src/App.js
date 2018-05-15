c45dfa38c8ed1330d107cb27466a59ab04a108ac
GabeCr4zy <slafer_gabriel@hotmail.com>
2018-05-15 16:54:31 -0300

    pagina de loguin e cadastro demoday

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Loguin from './Loguin/Loguin.js'; 
+
+class App extends Component {
+  render() {
+    return (
+     <Loguin/>
+    );
+  }
+}
+
+export default App;
